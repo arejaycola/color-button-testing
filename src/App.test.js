e7cbf27aff6bb209b00ec0a7855d77bf6c0a7662
Rob Mastronicola <arejaycola@gmail.com>
2021-08-18 14:22:15 -0400

    test and code function to transform camel case

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import { render, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
+import { replaceCamelWithSpaces } from './App';
+
 test('button has correct initial color', async () => {
 	render(<App />);
 
@@ -93,4 +95,17 @@ test('button turns gray when disabled', async () => {
 
 	expect(colorButton).toBeEnabled();
 	expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+});
+
+describe('spaces before camel-case capital letters', () => {
+	test('Works for no inner capital letters', () => {
+		expect(replaceCamelWithSpaces('Red')).toBe('Red');
+	});
+
+	test('Works for one inner capital letter', () => {
+		expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+	});
+
+	test('Works for multiple inner capital letters', () => {});
+	expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
 });
